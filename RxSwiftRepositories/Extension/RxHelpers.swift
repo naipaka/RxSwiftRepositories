f821186b4dad9788274759ab0d99e458a46239ed
naipaka <naipakapaka@gmail.com>
2020-09-17 12:19:52 +0900

    Add useful extension

--- RxSwiftRepositories/Extension/RxHelpers.swift
+++ RxSwiftRepositories/Extension/RxHelpers.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+extension ObservableType where Element: OptionalType {
+    func filterNil() -> Observable<Element.Wrapped> {
+        return self.flatMap { element -> Observable<Element.Wrapped> in
+            guard let value = element.value else {
+                return Observable<Element.Wrapped>.empty()
+            }
+            return Observable<Element.Wrapped>.just(value)
+        }
+    }
+}
+
+protocol OptionalType {
+    associatedtype Wrapped
+    var value: Wrapped? { get }
+}
+
+extension Optional: OptionalType {
+    public var value: Wrapped? {
+        return self
+    }
+}
+
+extension Reactive where Base: UIScrollView {
+    var reachedBottom: ControlEvent<Void> {
+        let observable = contentOffset
+            .flatMap { [weak base] contentOffset -> Observable<Void> in
+                guard let scrollView = base else { return Observable.empty() }
+
+                let visibleHeight = scrollView.frame.height - scrollView.contentInset.top - scrollView.contentInset.bottom
+                let y = contentOffset.y + scrollView.contentInset.top
+                let threshold = max(0.0, scrollView.contentSize.height - visibleHeight)
+
+                return y > threshold ? Observable.just(()) : Observable.empty()
+        }
+        return ControlEvent(events: observable)
+    }
+}
--- RxSwiftRepositories/Extension/SessionExtension.swift
+++ RxSwiftRepositories/Extension/SessionExtension.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+import RxSwift
+import APIKit
+
+extension Session: ReactiveCompatible {}
+
+extension Reactive where Base: Session {
+    func response<T: Request>(_ request: T) -> Observable<T.Response> {
+        return Observable<T.Response>.create { [weak base] observer -> Disposable in
+            #if DEBUG
+            print("------------ Start API Request ------------")
+            print("url == \(request.baseURL.absoluteString)\(request.path)")
+            if let parameters = request.parameters {
+                print("parameters == \(parameters)")
+            }
+            if let body = request.bodyParameters as? MultipartFormDataBodyParameters {
+                print("body parameters")
+                body.parts.forEach {
+                    print("\($0.name) == \($0.inputStream)")
+                }
+            }
+            print("-------------------------------------------")
+            #endif
+            let task = base?.send(request) { result in
+                switch result {
+                case .success(let value):
+                    #if DEBUG
+                    print("------------ Success API Request ------------")
+                    print("\(value)")
+                    print("---------------------------------------------")
+                    #endif
+
+                    observer.onNext(value)
+                    observer.onCompleted()
+                case .failure(let error):
+                    observer.onError(error)
+                }
+            }
+            return Disposables.create { task?.cancel() }
+        }
+    }
+}
